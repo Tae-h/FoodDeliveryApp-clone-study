c6e36a40d55c0a6c6ad450f2fc767720f79b55cb
Tae-h <zppy13@gmail.com>
2022-06-14 22:15:22 +0900

    [react-native] styles 속성

--- App.tsx
+++ App.tsx
@@ -4,8 +4,15 @@ import {
   createNativeStackNavigator,
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
-import {Text, TouchableHighlight, View} from 'react-native';
-import {useCallback} from 'react';
+import {
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
+import {useCallback, useState} from 'react';
 
 type RootStackParamList = {
   Home: undefined;
@@ -59,6 +66,8 @@ function DetailsScreen({navigation}: DetailsScreenProps) {
 const Stack = createNativeStackNavigator();
 
 function App() {
+  const [modal, showModal] = useState(true);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -71,8 +80,51 @@ function App() {
           {props => <DetailsScreen {...props} />}
         </Stack.Screen>
       </Stack.Navigator>
+      {modal && (
+        <Pressable onPress={() => showModal(true)} style={styles.modal}>
+          <View style={styles.modalInner}>
+            <View style={{flex: 1, backgroundColor: 'yellow'}}>
+              <Text>모달 부분</Text>
+            </View>
+            <View
+              style={{flexDirection: 'row', height: 50, alignItems: 'center'}}>
+              <Pressable style={{flex: 1, alignItems: 'center'}}>
+                <Text>Ok!</Text>
+              </Pressable>
+              <Pressable style={{flex: 1, alignItems: 'center'}}>
+                <Text>No!</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Pressable>
+      )}
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  modal: {
+    backgroundColor: 'rgba(0,0,0, 0.5)',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+  },
+  modalInner: {
+    backgroundColor: 'orange',
+    borderRadius: 10,
+    /*top: 50,
+    bottom: 50,*/
+    height: 300,
+    /*right: 50,
+    left: 50,*/
+    marginHorizontal: 50,
+    width: Dimensions.get('window').width - 100,
+    padding: 20,
+    shadowColor: 'black',
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    shadowOffset: {width: 5, height: 5},
+    elevation: 15,
+  },
+});
 
 export default App;
