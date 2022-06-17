7c1f38fd4891113fc8e9bafad476bdefb803482b
Tae-h <zppy13@gmail.com>
2022-06-17 16:09:17 +0900

    [react-native] 페이지

--- App.tsx
+++ App.tsx
@@ -1,130 +1,68 @@
 import * as React from 'react';
-import {NavigationContainer, ParamListBase} from '@react-navigation/native';
-import {
-  createNativeStackNavigator,
-  NativeStackScreenProps,
-} from '@react-navigation/native-stack';
-import {
-  Dimensions,
-  Pressable,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-} from "react-native";
-import {useCallback, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Settings from './src/pages/Settings';
+import Orders from './src/pages/Orders';
+import Delivery from './src/pages/Delivery';
+import {useState} from 'react';
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
 
-type RootStackParamList = {
-  Home: undefined;
-  Details: undefined;
+export type LoggedInParamList = {
+  Orders: undefined;
+  Settings: undefined;
+  Delivery: undefined;
+  Complete: {orderId: string};
 };
 
-type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
-type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
+export type RootStackParamList = {
+  SignIn: undefined;
+  SignUp: undefined;
+};
 
-function HomeScreen({navigation}: HomeScreenProps) {
-  const onClick = useCallback(() => {
-    navigation.navigate('Details');
-  }, [navigation]);
-
-  return (
-    <>
-      <View style={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
-        <TouchableHighlight onPress={onClick}>
-          <Text>Home Screen</Text>
-        </TouchableHighlight>
-      </View>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'yellow',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Text>test</Text>
-      </View>
-    </>
-  );
-}
-
-function DetailsScreen({navigation}: DetailsScreenProps) {
-  const onClick = useCallback(() => {
-    navigation.navigate('Home');
-  }, [navigation]);
-
-  return (
-    <>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <TouchableHighlight onPress={onClick}>
-          <Text>Details Screen</Text>
-        </TouchableHighlight>
-      </View>
-    </>
-  );
-}
-
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const [modal, showModal] = useState(true);
+  const [isLoggedIn, setLoggedIn] = useState(false);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: '홈화면', headerShown: true}}
-        />
-        <Stack.Screen name="Details">
-          {props => <DetailsScreen {...props} />}
-        </Stack.Screen>
-      </Stack.Navigator>
-      {modal && (
-        <Pressable onPress={() => showModal(true)} style={styles.modal}>
-          <View style={styles.modalInner}>
-            <View style={{flex: 1, backgroundColor: 'yellow'}}>
-              <Text>모달 부분</Text>
-            </View>
-            <View
-              style={{flexDirection: 'row', height: 50, alignItems: 'center'}}>
-              <Pressable style={{flex: 1, alignItems: 'center'}}>
-                <Text>Ok!</Text>
-              </Pressable>
-              <Pressable style={{flex: 1, alignItems: 'center'}}>
-                <Text>No!</Text>
-              </Pressable>
-            </View>
-          </View>
-        </Pressable>
+      {isLoggedIn ? (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Orders"
+            component={Orders}
+            options={{title: '오더 목록'}}
+          />
+          <Tab.Screen
+            name="Delivery"
+            component={Delivery}
+            options={{headerShown: false}}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{title: '내 정보'}}
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{title: '로그인'}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{title: '회원가입'}}
+          />
+        </Stack.Navigator>
       )}
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  modal: {
-    backgroundColor: 'rgba(0,0,0, 0.5)',
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'center',
-  },
-  modalInner: {
-    backgroundColor: 'orange',
-    borderRadius: 10,
-    /*top: 50,
-    bottom: 50,*/
-    height: 300,
-    /*right: 50,
-    left: 50,*/
-    marginHorizontal: 50,
-    width: Dimensions.get('window').width - 100,
-    padding: 20,
-    shadowColor: 'black',
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
-    shadowOffset: {width: 5, height: 5},
-    elevation: 15,
-  },
-});
 
 export default App;
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,137 @@
+import {
+  Alert,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+} from 'react-native';
+import React, {useCallback, useRef, useState} from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../App';
+
+type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
+
+function SignIn({navigation}: SignInScreenProps) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const emailRef = useRef<TextInput | null>(null);
+  const passwordRef = useRef<TextInput | null>(null);
+
+  const onChangeEmail = useCallback((text: any) => {
+    // e.target.value  아님 text가 바로 들어 있음!!
+    setEmail(text);
+  }, []);
+
+  const onChangePassword = useCallback((text: any) => {
+    // e.target.value  아님 text가 바로 들어 있음!!
+    setPassword(text);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    if (!email || !email.trim()) {
+      return Alert.alert('알림', '이메일을 입력해 주세요.');
+    }
+
+    if (!password || !password.trim()) {
+      return Alert.alert('알림', '비밀번호를 입력해 주세요.');
+    }
+
+    Alert.alert('알림', '로그인 되었습니다.!');
+  }, [email, password]);
+
+  const toSignUp = useCallback(() => {
+    navigation.navigate('SignUp');
+  }, [navigation]);
+
+  const canGoNext = email && password;
+
+  return (
+    <View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>이메일</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder={'이메일을 입력해 주세요.'}
+          value={email}
+          onChangeText={onChangeEmail}
+          importantForAutofill={'yes'} // 디바이스 저장
+          autoComplete="email"
+          textContentType="emailAddress"
+          keyboardType={'email-address'} // 키보드에 @ 보여줌
+          ref={emailRef}
+          onSubmitEditing={() => {
+            passwordRef.current?.focus();
+          }}
+          blurOnSubmit={true}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>비밀번호</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder={'비밀번호를 입력해 주세요.'}
+          value={password}
+          onChangeText={onChangePassword}
+          secureTextEntry
+          importantForAutofill={'yes'} // 디바이스 비밀번호 저장
+          autoComplete="password"
+          textContentType="password"
+          ref={passwordRef}
+          onSubmitEditing={onSubmit}
+        />
+      </View>
+      <View style={styles.buttonZone}>
+        <Pressable
+          onPress={onSubmit}
+          style={
+            !canGoNext
+              ? styles.loginButton
+              : /*: [styles.loginButton, styles.loginButtonActive]*/
+                StyleSheet.compose(styles.loginButton, styles.loginButtonActive)
+          }
+          disabled={!canGoNext}>
+          <Text style={styles.loginButtonText}>로그인</Text>
+        </Pressable>
+        <Pressable onPress={toSignUp}>
+          <Text>회원가입</Text>
+        </Pressable>
+      </View>
+      <View />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  textInput: {
+    padding: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  inputWrapper: {
+    padding: 20,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  loginButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  loginButtonActive: {
+    backgroundColor: 'blue',
+  },
+  buttonZone: {
+    alignItems: 'center',
+  },
+});
+
+export default SignIn;
